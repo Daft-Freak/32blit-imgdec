f4ade45dc47df0af811ad59be7a13c4280d1a39a
Charlie Birks <charlie@daft.games>
2024-10-17 17:27:01 +0100

    Redefine bswap for MSVC

--- not-duino/Arduino.h
+++ not-duino/Arduino.h
@@ -2,9 +2,14 @@
 
 #pragma once
 #include <cstdint>
+#include <cstdlib>
 #include <cstring>
 
 #ifdef __ARM_FEATURE_UNALIGNED
 #define ALLOWS_UNALIGNED
 #endif
 
+#ifdef _MSC_VER
+#define __builtin_bswap16(x) _byteswap_ushort(x)
+#define __builtin_bswap32(x) _byteswap_ulong(x)
+#endif
